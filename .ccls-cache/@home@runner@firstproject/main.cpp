8b90a243efddcfe24da4ac050e2a42ac293247e7
UKRAINE20 <150735835+UKRAINE20@users.noreply.github.com>
2024-02-20 12:09:57 +0000

    mass_1

--- .ccls-cache/@home@runner@firstproject/main.cpp
+++ .ccls-cache/@home@runner@firstproject/main.cpp
@@ -14,5 +14,10 @@ int main() {
     cout << "Введіть " << i + 1 << " елемент масиву\n";
     cin >> array[i];
   }
+  cout << "Вміст масиву:\n";
+  for (int i = 0; i < SIZE; i++) {
+      cout << array[i] << " ";
+  }
+  cout << endl;
   return 0;
 }
